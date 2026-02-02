8f17ef049ca2bfeed4cffeaea6da852e3b505d7e
Alok Singh <thealoksinghh@gmail.com>
2026-02-02 22:41:01 +0530

    now new deadline is 20 feb

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(function () {
-    var releaseDate = '02/20/2026 07:07:07';
+    var now = new Date();
+    var day = now.getDate();
+    var month = now.getMonth();
+    var year = now.getFullYear() + 1;
+
+    var nextyear = month + '/' + day + '/' + year + ' 07:07:07';
 
     $('#timer').countdown({
-        date: releaseDate, //Website release date: February 20, 2026
+        date: nextyear, //TODO Date format: 07/27/2017 17:00:00
         offset: +2,
         day: 'Day',
         days: 'Days'
